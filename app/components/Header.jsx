4987e7e64c0caa364b6f35c9bb8718f6a102e05a
BaoMinhHuynh <huynhbaominh.h1.14cla@gmail.com>
2026-03-18 15:46:50 +0700

    update CV

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -53,7 +53,7 @@ const Header = () => {
           initial={{ y: 30, opacity: 0 }}
           whileInView={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.6, delay: 1.2 }}
-          href="/HuynhBaoMinh_CV.pdf"
+          href="/HuynhBaoMinh_CV_Data-Analyst.pdf"
           download
           className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2"
         >
